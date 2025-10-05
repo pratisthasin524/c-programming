826939851f428fe4e92a3853fb4935a223851a29
Pratistha Singh <pratisthasingh1711@gmail.com>
2025-10-05 18:15:54 +0530

    area_circum.c

---
+++ area_circum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.1416
+int main() {
+    float radius, area, circumference;
+    printf("Enter radius of the circle: ");
+    scanf("%f", &radius);
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+    printf("Radius = %.2f\n", radius);
+    printf("Area of Circle = %.2f\n", area);
+    printf("Circumference of Circle = %.2f", circumference);
+    return 0;
+}
